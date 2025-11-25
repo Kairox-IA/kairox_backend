c24807bc20408fc667ddd988a7d269d1f5735f4a
Kairox-IA <iakairox1@gmail.com>
2025-11-25 16:18:13 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const cors = require('cors');
 const clientesRoutes = require('./routes/clientes');
-require('dotenv').config();
+require('./db');
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-// Rutas
-app.use('/api/clientes', clientesRoutes);
-
-// Ruta por defecto
+// Ruta raíz
 app.get('/', (req, res) => {
-  res.send('API Kairox funcionando');
+  res.send('Backend Kairox funcionando en Render');
 });
 
+// Rutas API
+app.use('/api/clientes', clientesRoutes);
+
+// Puerto dinámico
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor ejecutándose en puerto ${PORT}`);
+  console.log(`Servidor en puerto ${PORT}`);
 });
